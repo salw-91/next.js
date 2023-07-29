d2ddd861955002983a398f5a357f5d798745397f
Salwan-Aldulaimi <salwan@holygrow.nl>
2023-07-29 07:04:13 +0200

    Create DB Config

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,22 @@
+import mongoose, { Connection, connection } from "mongoose";
+
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGO_URI!)
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully')
+        })
+
+        connection.on('error', (err) => {
+            console.log('MongoDB Connection error. Please make suer MongoDB is running.' + err)
+            process.exit();
+        })
+        
+    } catch (error) {
+        console.log('Something goes wrong!')
+        console.log(error)
+    }
+    
+}
