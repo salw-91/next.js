9673d53b13bec2b2f1bba19cd682d5cf01913457
Salwan-Aldulaimi <salwan@holygrow.nl>
2023-08-04 06:39:39 +0200

    Add function to signup the user

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,8 @@
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Axios } from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function SigunpPage() {
   const router = useRouter();
@@ -12,8 +13,23 @@ export default function SigunpPage() {
     username: "",
   });
   const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
-  const onSignup = async () => {};
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+
+      console.log("Signup success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup failed", error.message);
+
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
@@ -25,7 +41,7 @@ export default function SigunpPage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Signup</h1>
+      <h1>{loading ? "Processing" : "Signup"}</h1>
       <hr />
       <label htmlFor="username">username</label>
       <input
